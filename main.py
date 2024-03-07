9b40095ab67150757397fd917ed028fbe6fd8422
Dmitry Noskov <windn12@gmail.com>
2024-03-07 12:13:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
 
+
 app = FastAPI()
-translate = pipeline('translation_en_to_ru', model='Helsinki-NLP/opus-mt-en-ru')
+translate = pipeline("translation_en_to_ru", model="Helsinki-NLP/opus-mt-en-ru")
 
 
 class Item(BaseModel):
     text: str
 
 
-@app.get('/')
+@app.get("/")
 def root():
-    return {'message': 'Hello World!!!'}
+    return {"message": "Hello World!!!"}
 
 
-@app.post('/predict/')
+@app.post("/predict/")
 def predict(item: Item):
     """Перевод текста"""
     return translate(item.text)
